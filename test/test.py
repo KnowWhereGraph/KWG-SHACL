6638393e6db97e1b620a53bef93bc612aedbc0d1
Cogan Shimizu <cog-academics@coganshimizu.com>
2021-10-14 20:20:15 -0500

    updated test framework to run on ObservationConstraint instead

--- test/test.py
+++ test/test.py
@@ -8,39 +8,41 @@ example_graph_paths = dict()
 
 labels = ["node", "property"]
 for label in labels:
-      directory = f"../shapes/{label}-shapes"
-      for node_shape in os.listdir(directory):
-            node_shape_path = os.path.join(directory, node_shape)
-            shape_paths += [node_shape_path]
-            filename = node_shape.replace("Constraint", "-ExampleGraph")
-            example_graph_path = os.path.join("./example-graphs", filename)
-            example_graph_paths[node_shape_path] = example_graph_path
+    directory = f"../shapes/{label}-shapes"
+    for node_shape in os.listdir(directory):
+        node_shape_path = os.path.join(directory, node_shape)
+        shape_paths += [node_shape_path]
+        filename = node_shape.replace("Constraint", "-ExampleGraph")
+        example_graph_path = os.path.join("./example-graphs", filename)
+        example_graph_paths[node_shape_path] = example_graph_path
 
 for shape_path in shape_paths:
-      if shape_path != "../shapes/property-shapes/observedPropertyConstraint.ttl":
-            continue
+    if shape_path != "../shapes/node-shapes/ObservationConstraint.ttl":
+        continue
 
-      # Load the data to validate
-      shape = Graph()
-      shape.parse(shape_path, format="ttl")
+    # Load the data to validate
+    shape = Graph()
+    shape.parse(shape_path, format="ttl")
 
-      # Get the shape to test
-      example_graph = Graph()
-      example_graph_path = example_graph_paths[shape_path]
-      example_graph.parse(example_graph_path, format='ttl')
+    # Get the shape to test
+    example_graph = Graph()
+    example_graph_path = example_graph_paths[shape_path]
+    example_graph.parse(example_graph_path, format='ttl')
 
-      # Validate
-      r = validate(data_graph = example_graph,
-            shacl_graph=  shape,
-            abort_on_first=False,
-            allow_warnings=False,
-            meta_shacl=False,
-            advanced=False,
-            js=False,
-            debug=False)
-      conforms, results_graph, results_text = r
+    # Validate
+    r = validate(
+        data_graph=example_graph,
+        shacl_graph=shape,
+        abort_on_first=False,
+        allow_warnings=False,
+        meta_shacl=True,
+        advanced=False,
+        js=False,
+        inference='rdfs',
+        serialize_report_graph=True)
+    conforms, results_graph, results_text = r
 
-      shape_name = shape_path.split("/")[-1]
-      print (f"Processing: {shape_name}!")
-      print (f"Result Text: {results_text}")
-      print (("#"*130 +"\n")*2)
+    shape_name = shape_path.split("/")[-1]
+    print (f"Processing: {shape_name}!")
+    print (f"Result Text: {results_text}")
+    print (("#"*130 +"\n")*2)
